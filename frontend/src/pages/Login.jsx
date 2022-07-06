4ddaf6ae5e7dc078c016079c45c1b10e07ac6c2a
kn8a <88045655+kn8a@users.noreply.github.com>
2022-07-06 14:29:36 +0700

    login and register pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,69 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
 
 function Login() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+      })
+
+    //destructure fields
+    const { email, password } = formData
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+          ...prevState,
+          [e.target.name]: e.target.value,
+        }))
+      }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
   return (
-    <div>Login</div>
+    <>
+        <section className='heading'>
+            <h1>
+                <FaSignInAlt/> Register
+            </h1>
+            <p>
+                Please create an account
+            </p>
+        </section>
+        <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              placeholder='Enter your email'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              placeholder='Enter password'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Login
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
