f66f287daa1ea28e65a61ebb3f9fa8126d1b111d
kn8a <88045655+kn8a@users.noreply.github.com>
2022-07-06 12:07:02 +0700

    react router, header and pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Dashbooard from './pages/Dashbooard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Header from './components/Header';
+
 function App() {
   return (
-    <div>
-      <h1>My app</h1>
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Header/>
+          <Routes>
+            <Route path='/' element={<Dashbooard/>}/>
+            <Route path='login' element={<Login/>}/>
+            <Route path='register' element={<Register/>}/>
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
