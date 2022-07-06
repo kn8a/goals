5d10c02d9daff355a99d699fdc8620ab8c815fd1
kn8a <88045655+kn8a@users.noreply.github.com>
2022-07-06 12:06:44 +0700

    setup react routes, header, and pages

---
+++ frontend/src/pages/Dashbooard.jsx
@@ -0,0 +1,22 @@
+
+//rfce snippet:
+
+// import React from 'react'
+
+// function Dashbooard() {
+//   return (
+//     <div>Dashbooard</div>
+//   )
+// }
+
+// export default Dashbooard
+
+import React from 'react'
+
+function Dashbooard() {
+  return (
+    <div>Dashbooard</div>
+  )
+}
+
+export default Dashbooard
