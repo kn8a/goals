1aedbc893e4b30d565bff8040777efea33fb6ac6
kn8a <88045655+kn8a@users.noreply.github.com>
2022-07-07 16:32:21 +0700

    login function

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -13,6 +13,16 @@ const register = async(userData) => {
     return response.data
 }
 
+//login user
+const login = async(userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+
+    if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
 
 //logout user
 const logout = () => {
@@ -21,7 +31,7 @@ const logout = () => {
 
 
 const authService = {
-    register, logout
+    register, logout, login
 }
 
 export default authService
