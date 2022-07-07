b293db407b94649156d50fbe377828225577eebb
kn8a <88045655+kn8a@users.noreply.github.com>
2022-07-07 16:10:53 +0700

    logout funtion

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -13,8 +13,15 @@ const register = async(userData) => {
     return response.data
 }
 
+
+//logout user
+const logout = () => {
+    localStorage.removeItem('user')
+}
+
+
 const authService = {
-    register
+    register, logout
 }
 
 export default authService
